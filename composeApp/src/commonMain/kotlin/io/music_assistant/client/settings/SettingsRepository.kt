c2a1fb634ce4602746ccfc1526deb3a35a4a2390
Stef Tervelde <stef@steftervelde.nl>
2026-09-24 15:29:00 +0200

    fix test 2

--- composeApp/src/commonMain/kotlin/io/music_assistant/client/settings/SettingsRepository.kt
+++ composeApp/src/commonMain/kotlin/io/music_assistant/client/settings/SettingsRepository.kt
@@ -13,7 +13,6 @@ import io.music_assistant.client.data.model.client.SortOption
 import io.music_assistant.client.data.model.client.SubItemContext
 import io.music_assistant.client.ui.theme.ThemeSetting
 import io.music_assistant.client.utils.myJson
-import io.music_assistant.client.utils.platformDeviceName
 import io.music_assistant.sendspin.api.AudioCodec
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
